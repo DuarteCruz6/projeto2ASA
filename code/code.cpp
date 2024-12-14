367fde639772adec46165417b0a927fa0f36176c
Duarte Cruz <dudu.cruz2005@gmail.com>
2024-12-14 18:42:46 +0000

    added comments

--- code/code.cpp
+++ code/code.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Ligacao; 
 
 struct Estacao{
-    int id;
+    int id; //id da estação
     int numLinhas; //quantas linhas tem a estacao
     vector<Ligacao*> listaLigacoes; //vetor com todas as ligacoes desta estacao
     vector<int> listaLinhas; //vetor com todos os id's das linhas desta estacao
@@ -14,12 +14,12 @@ struct Estacao{
 struct Ligacao{
     Estacao* estacaoX; 
     Estacao* estacaoY;
-    int linha; //id da linha
+    int id_linha; //id da linha
 };
 
 int numEstacoesTotal, numLigacoesTotal, numLinhas;
-vector <Estacao*> listaEstacoes;
-vector <Ligacao*> listaLigacoes;
+vector <Estacao*> listaEstacoes; //lista global de todas as estações existentes
+vector <Ligacao*> listaLigacoes; //lista global de todas as ligações existentes
 
 //adiciona a linha à estação caso esta ainda não pertencesse
 void adicionarLinhaEstacao(Estacao* estacao, int id_linha){
@@ -29,8 +29,8 @@ void adicionarLinhaEstacao(Estacao* estacao, int id_linha){
         }
     }
     //a estacao ainda não tinha esta linha
-    estacao->listaLinhas.push_back(id_linha);
-    estacao->numLinhas++;
+    estacao->listaLinhas.push_back(id_linha); //adiciona à lista
+    estacao->numLinhas++; //aumenta o numero de linhas da estacao
 }
 
 //adiciona uma nova ligacao à estação
@@ -40,8 +40,8 @@ void addLigacao(Estacao* estacao, Ligacao* novaLigacao){
 
 //cria uma nova ligacao
 Ligacao* criarLigacao(Estacao* estacaoX, Estacao* estacaoY, int id_linha){
-    Ligacao* novaLigacao = new Ligacao{estacaoX,estacaoY, id_linha};
-    listaLigacoes.push_back(novaLigacao);
+    Ligacao* novaLigacao = new Ligacao{estacaoX,estacaoY, id_linha}; //cria a ligacao
+    listaLigacoes.push_back(novaLigacao); //adiciona a ligacao à lista global de ligações
     addLigacao(estacaoX,novaLigacao); //adiciona a ligacao às ligacoes da estacao X
     addLigacao(estacaoY,novaLigacao); //adiciona a ligacao às liigacoes da estacao Y
     return novaLigacao;
@@ -49,8 +49,8 @@ Ligacao* criarLigacao(Estacao* estacaoX, Estacao* estacaoY, int id_linha){
 
 //cria uma nova estacao e adiciona à lista de estacoes existentes
 Estacao* criarEstacao(int id_novaEstacao, int id_linha){
-    Estacao* novaEstacao = new Estacao{id_novaEstacao,1,{},{id_linha}};
-    listaEstacoes.push_back(novaEstacao);
+    Estacao* novaEstacao = new Estacao{id_novaEstacao,1,{},{id_linha}}; //cria a estação
+    listaEstacoes.push_back(novaEstacao); //adiciona a estação à lista global de estações
     return novaEstacao;
 }
 
@@ -59,12 +59,13 @@ Estacao* criarEstacao(int id_novaEstacao, int id_linha){
 Estacao* getEstacao(int id_estacao){
     for(Estacao* estacaoAtual : listaEstacoes){
         if(estacaoAtual->id==id_estacao){
-            return estacaoAtual;
+            return estacaoAtual; //a estacao existe, portanto retornamo-la
         }
     }
-    return NULL;
+    return NULL; //a estacao nao existe
 }
 
+//obtem os inputs e cria todas as entidades
 void getInput(){
     scanf("%d %d %d", &numEstacoesTotal, &numLigacoesTotal, &numLinhas);
     vector<bool> estacoesCriadas(numEstacoesTotal, false); //vetor de bools para verificar se a estacao ja foi ou nao criada 
@@ -77,20 +78,20 @@ void getInput(){
         Estacao* estacaoY;
         if(!estacoesCriadas[id_estacaoX-1]){
             //estacao X ainda não foi criada
-            estacaoX = criarEstacao(id_estacaoX,id_linha);
-            estacoesCriadas[id_estacaoX-1]=true;
-            numEstacoesCriadas++;
+            estacaoX = criarEstacao(id_estacaoX,id_linha); //cria a estacao X
+            estacoesCriadas[id_estacaoX-1]=true; //mete true para nao poder ser criada novamente
+            numEstacoesCriadas++; //aumenta o numero de estacoes criadas
         }else{
             //estacao X já tinha sido criada
-            estacaoX = getEstacao(id_estacaoX);
+            estacaoX = getEstacao(id_estacaoX); 
             adicionarLinhaEstacao(estacaoX,id_linha); //adiciona a linha à estacao caso ainda não a tenha
         }
         if(!estacoesCriadas[id_estacaoY-1]){
             //estacao Y ainda não foi criada
-            estacaoY = criarEstacao(id_estacaoY,id_linha);
-            estacoesCriadas[id_estacaoY-1]=true;
-            numEstacoesCriadas++;
-        }else{
+            estacaoY = criarEstacao(id_estacaoY,id_linha); //cria a estacao Y
+            estacoesCriadas[id_estacaoY-1]=true; //mete true para nao poder ser criada novamente
+            numEstacoesCriadas++; //aumenta o numero de estacoes criadas
+        }else{ 
             //estacao Y já tinha sido criada
             estacaoY = getEstacao(id_estacaoY);
             adicionarLinhaEstacao(estacaoY,id_linha); //adiciona a linha à estacao caso ainda não a tenha
@@ -99,13 +100,13 @@ void getInput(){
     }
 
     if(numEstacoesCriadas!=numEstacoesTotal){
-        //há uma estção sem ligação
+        //há pelo menos uma estação sem ligação
         printf("%d\n",-1);
         return;
     }
 }
 
 int main(){
-    getInput();
+    getInput(); //obtem os inputs e cria todas as entidades
     return 0;
 }
